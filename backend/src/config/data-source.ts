2362721a4b80965ef5cc89204bb00f3f984b240d
arturfurtado <artururtado@gmail.com>
2025-05-06 22:35:18 -0300

    moving files to its right directories

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,18 +1,28 @@
 import "reflect-metadata";
+import * as dotenv from "dotenv";
 import { DataSource } from "typeorm";
 import { Field } from "../entities/fields";
 import { Fill } from "../entities/fills";
-import * as dotenv from "dotenv";
-dotenv.config();
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
+  host:     process.env.DB_HOST,
+  port:     Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   entities: [Field, Fill],
   migrations: ["src/migration/*.ts"],
-  synchronize: false,    
+  synchronize: false,
+});
+
+console.log({
+  host:     process.env.DB_HOST,
+  port:     process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
