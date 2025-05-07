8968898a8d8d85472be0fa77f38d43ebd13a394c
arturfurtado <artururtado@gmail.com>
2025-05-07 16:51:51 -0300

    docs: swagger loaded in app.ts, swagger documentation

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 import fieldRouter from "./routes/fieldRoutes";
 import fillRouter from "./routes/fillRoutes";
+
+const swaggerDocument = YAML.load(
+  path.resolve(__dirname, "./docs/swagger.yaml")
+);
 
 interface HttpError extends Error {
   statusCode?: number;
@@ -12,17 +19,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/campos", fieldRouter);
 app.use("/preenchimentos", fillRouter);
 
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack || err);
-
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Ocorreu um erro interno no servidor.";
-
-  res.status(statusCode).json({ message });
-
-});
+app.use(
+  (err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    if (process.env.NODE_ENV !== "test") {
+      console.error(err.stack || err);
+    }
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Ocorreu um erro interno no servidor.";
+    res.status(statusCode).json({ message });
+  }
+);
 
 export default app;
