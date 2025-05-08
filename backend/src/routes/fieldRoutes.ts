6e6f5df92223e1321bfec9fbcdaff29977ab1c91
arturfurtado <artururtado@gmail.com>
2025-05-08 16:59:44 -0300

    feat: routes for update and delete

--- backend/src/routes/fieldRoutes.ts
+++ backend/src/routes/fieldRoutes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validateBody";
-import { createFieldSchema } from "../validators/fieldValidator";
+import {
+  createFieldSchema,
+  updateFieldSchema,
+} from "../validators/fieldValidator";
 import * as fieldCtrl from "../controllers/fieldController";
 
 const router = Router();
 
-router.post("/", validateBody(createFieldSchema), fieldCtrl.createField);
+router.post(
+  "/",
+  validateBody(createFieldSchema),
+  fieldCtrl.createField
+);
 router.get("/", fieldCtrl.getFields);
+
+router.put(
+  "/:id",
+  validateBody(updateFieldSchema),
+  fieldCtrl.updateField
+);
+
+router.delete("/:id", fieldCtrl.deleteField);
 
 export default router;
--- backend/src/routes/fillRoutes.ts
+++ backend/src/routes/fillRoutes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import { validateBody } from "../middlewares/validateBody";
-import { createFillSchema } from "../validators/fillValidator";
+import {
+  createFillSchema,
+  updateFillSchema,
+} from "../validators/fillValidator";
 import * as fillCtrl from "../controllers/fillController";
 
 const router = Router();
 
-router.post("/", validateBody(createFillSchema), fillCtrl.createFill);
+router.post(
+  "/",
+  validateBody(createFillSchema),
+  fillCtrl.createFill
+);
 router.get("/", fillCtrl.getFills);
+
+router.put(
+  "/:id",
+  validateBody(updateFillSchema),
+  fillCtrl.updateFill
+);
+
+router.delete("/:id", fillCtrl.deleteFill);
 
 export default router;
